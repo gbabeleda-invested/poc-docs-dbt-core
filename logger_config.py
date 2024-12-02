ec0d5837af4e03d01bc42ece840ce80f6adcfb91
Gio Abeleda <gio.abeleda@invested.ph>
2024-12-02 14:28:22 +0800

    Logger now writes to logs folder to be in line with DBT

--- logger_config.py
+++ logger_config.py
@@ -1,12 +1,16 @@
 import logging 
+import os 
 
 def setup_logger() -> None:
+    log_dir = "logs"
+    os.makedirs(log_dir, exist_ok=True)
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         handlers=[
             logging.StreamHandler(),
-            logging.FileHandler('poc_docs.log')
+            logging.FileHandler(os.path.join(log_dir, 'other_funcs.log'))
         ],
         force=True
     )
