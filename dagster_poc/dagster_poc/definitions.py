a68cf139f0797d30815f30a8d4f0fa77c9c07438
Gio Abeleda <gio.abeleda@invested.ph>
2024-12-02 18:22:08 +0800

    Added DBT resource to definitions objects

--- dagster_poc/dagster_poc/definitions.py
+++ dagster_poc/dagster_poc/definitions.py
@@ -1,13 +1,14 @@
 from dagster import Definitions, load_assets_from_modules
 
 from . import assets  # noqa: TID252
-from .resources import poc_s3_resource
+from .resources import poc_s3_resource, dbt_resource
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
     resources={
-        "s3" : poc_s3_resource
+        "s3" : poc_s3_resource,
+        "dbt" : dbt_resource
     }
 )
